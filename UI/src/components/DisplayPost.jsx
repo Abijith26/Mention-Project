c45948ca21323a745cf6f51b9cad0517a2332a31
Abijith26 <abimurugan.c@gmail.com>
2024-05-17 16:19:14 +0530

    Final Changes

--- UI/src/components/DisplayPost.jsx
+++ UI/src/components/DisplayPost.jsx
@@ -1,48 +1,25 @@
-const PostingUser = {name: "Abijith Murugan"};
-
-import { useEffect, useState } from 'react';
+const PostingUser = { name: "Abijith Murugan" };
 
 export default function DisplayPost({ postMessage, taggedUser }) {
-//   const [postContent, setPostContent] = useState([]);
-
-//   useEffect(() => {
-//     if (postMessage && taggedUser) {
-//       setPostContent((current) => [
-//         { message: {postMessage}, user: {taggedUser} },
-//         ...current,
-//       ]);
-//     }
-//   }, []);
-
-//   console.log(postContent);
-
-  // const postPresent = postMessage || taggedUser;
-
   return (
-
-    // <div className='mt-5'>
-    //     {postContent.length === 0 && <p className='text-white'>No Posts</p>}
-    //   {postContent.map((post, index) => (
-    //     <div key={index}>
-    //       <p className='text-gray-600'>
-    //         {post.message} <span className='text-fuchsia-600 font-semibold'>{post.user}</span>
-    //       </p>
-    //     </div> 
-    //   ))}
-    // </div>
-  
-   
     <div className="text-slate-500  p-7 bg-slate-300  rounded-md">
-      <p>{postMessage} { taggedUser && <span className='text-fuchsia-500 font-bold'>{taggedUser}</span>}</p>
-    
+      <p>
+        {postMessage}{" "}
+        {taggedUser && (
+          <span className="text-fuchsia-500 font-bold">{taggedUser}</span>
+        )}
+      </p>
+
       <hr className="border-gray-700 mt-10"></hr>
       <div className="mt-2 flex p-4 gap-5">
-        <span className='bg-red-600  rounded-full w-9 h-9 text-center font-semibold p-1 text-black'>{PostingUser.name.at(0)}</span>
-        <div className='flex flex-col gap-2'>
-          <span className='text-black'>{PostingUser.name}</span>
+        <span className="bg-red-600  rounded-full w-9 h-9 text-center font-semibold p-1 text-black">
+          {PostingUser.name.at(0)}
+        </span>
+        <div className="flex flex-col gap-2">
+          <span className="text-black">{PostingUser.name}</span>
           <span>1 Hour Ago</span>
         </div>
       </div>
     </div>
-);
+  );
 }
--- UI/src/components/MentionSelect.jsx
+++ UI/src/components/MentionSelect.jsx
@@ -1,49 +1,85 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const users = ['Tommy','Steve','Danny','Alex','Jones','Charlie','Alexis','Cherie','Reagan','Lacy','Angela','Codi','Gia','Gianna','Luna','Dee'];
+// Sample Users Array
+const users = [
+  "Tommy",
+  "Steve",
+  "Danny",
+  "Alex",
+  "Jones",
+  "Charlie",
+  "Alexis",
+  "Cherie",
+  "Reagan",
+  "Lacy",
+  "Angela",
+  "Codi",
+  "Gia",
+  "Gianna",
+  "Luna",
+  "Dee",
+];
 
-
-export default function MentionSelect({taggedUser, setTaggedUser, setIsAtSymbol, cursorPosition, setMessage}) {
+export default function MentionSelect({
+  setTaggedUser,
+  setIsAtSymbol,
+  cursorPosition,
+  setMessage,
+}) {
   const [filteredUsers, setFilteredUsers] = useState([]);
-  
-  
+
   const handleInputChange = (e) => {
     // Getting the Input Value
     const inputValue = e.target.value;
 
-    if(inputValue !== ''){
-
-        const result = users.filter((user) =>
-            user.toLowerCase().includes(inputValue.toLowerCase()));
-        setFilteredUsers([...result]);
-    }else{
-        setFilteredUsers([]);
+    if (inputValue !== "") {
+      const result = users.filter((user) =>
+        user.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      setFilteredUsers([...result]);
+    } else {
+      setFilteredUsers([]);
     }
+  };
 
-}
-
-function HandleTag(e){
-    setMessage((current)=> current.split("@")[0])
+  function HandleTag(e) {
+    setMessage((current) => current.split("@")[0]);
     setTaggedUser(e.target.value);
-    
+
     setIsAtSymbol(false);
-}
+  }
 
-  
   return (
-    <div className="w-auto h-auto max-h-48 border-solid border-2 border-black absolute  z-20 top-1 p-2 bg-white overflow-y-auto overflow-x-hidden" style={{ left: `${cursorPosition.top}rem` }}>
-    <input type="text" className="rounded-md p-1" onChange={handleInputChange} />
-    { filteredUsers.length > 0 && <div className="mt-2 z-20">
-
-    {filteredUsers.map((user)=>(
-        <div className="flex justify-center items-center gap-7 p-2 text-black hover:bg-slate-600 hover:text-white" key={user}>
-        <span className='bg-red-600  rounded-full w-9 h-9 text-center font-semibold p-1'>{user.at(0)}</span>
-        <option className='font-semibold hover:cursor-pointer mt-1' value={user} onClick={HandleTag}>{user}</option>
+    <div
+      className="w-auto h-auto max-h-48 border-solid border-2 border-black absolute  z-20 top-1 p-2 bg-white overflow-y-auto overflow-x-hidden"
+      style={{ left: `${cursorPosition.left}rem` }}
+    >
+      <input
+        type="text"
+        className="rounded-md p-1"
+        onChange={handleInputChange}
+      />
+      {filteredUsers.length > 0 && (
+        <div className="mt-2 z-20">
+          {filteredUsers.map((user) => (
+            <div
+              className="flex justify-center items-center gap-7 p-2 text-black hover:bg-slate-600 hover:text-white"
+              key={user}
+            >
+              <span className="bg-red-600  rounded-full w-9 h-9 text-center font-semibold p-1">
+                {user.at(0)}
+              </span>
+              <option
+                className="font-semibold hover:cursor-pointer mt-1"
+                value={user}
+                onClick={HandleTag}
+              >
+                {user}
+              </option>
+            </div>
+          ))}
         </div>
-    ))}
-    </div>}
-  
-</div>
-)
-  
+      )}
+    </div>
+  );
 }
